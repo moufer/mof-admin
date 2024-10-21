4164303dc5b07d90520980683d8823d6501fd20d
moufer <moufer@163.com>
2024-10-21 21:23:15 +0800

    文字调整

--- public/admin/app/system/login.js
+++ public/admin/app/system/login.js
@@ -14,8 +14,8 @@ export default {
     const router = useRouter();
 
     const http = inject('http');
-    const loginTitle = inject('loginTitle', '磨锋AIGC系统');
-    const loginModule = inject('loginModule', 'system');
+    const loginTitle = inject('loginTitle', '磨锋后台管理');
+    const loginModule = inject('loginModule', '磨锋后台管理');
 
     const formValues = reactive({ username: '', password: '', module: loginModule });
     const loginRules = {
